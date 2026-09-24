c32997da2f8ce003d2afde6a3830eadefbe9b826
ChromaEsport <max-kaya@live.fr>
2026-09-24 13:51:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ document.addEventListener("DOMContentLoaded", async () => {
       container.innerHTML = `
         <img
           src="logo-twitch.png"
-          style="width:16px;height:16px;vertical-align:middle;margin-right:4px;"
+          style="width:16px;height:20px;vertical-align:middle;margin-right:2px;"
         >
         ${info.event.title}
         (${info.event.extendedProps.start}-${info.event.extendedProps.end})
